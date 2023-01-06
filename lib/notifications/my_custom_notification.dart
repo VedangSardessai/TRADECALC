9b90f381a312df4086326f66959566166c491a09
VedangSardessai <vms112000@gmail.com>
2023-01-07 01:11:01 +0530

    code:local notifications done

--- lib/notifications/my_custom_notification.dart
+++ lib/notifications/my_custom_notification.dart
@@ -1,6 +1,34 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-class MyNotifications{
+class MyNotifications {
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
+  final AndroidInitializationSettings _androidInitializationSettings =
+      const AndroidInitializationSettings('logo');
 
+  void notificationInitialization() async {
+    InitializationSettings initializationSettings = InitializationSettings(
+      android: _androidInitializationSettings,
+    );
+
+    _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+    print('Initialization done');
+  }
+
+  void showTextNotification(String title, String body) async {
+    AndroidNotificationDetails androidNotificationDetails =
+        const AndroidNotificationDetails(
+      'channelId',
+      'channelName',
+      priority: Priority.high,
+      importance: Importance.max,
+    );
+
+    NotificationDetails notificationDetails =
+        NotificationDetails(android: androidNotificationDetails);
+
+    await _flutterLocalNotificationsPlugin.show(
+        0, title, body, notificationDetails);
+  }
 }
